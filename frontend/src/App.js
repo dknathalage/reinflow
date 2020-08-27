8d34ae1e997ae048f0ec92601cc04c4a665a9065
Sheron Suditha <suditha@gamer.lk>
2020-08-27 14:29:44 +0530

    ENHANCEMENT: UI 1.0 Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import logo from './assets/REINFLOW.png';
 import './App.css';
+import AdminHome from './admin';
+
 function App() {
-	return (
-		<div className="App">
-			<div className="container">
-				<img src={logo} alt="" />
-				<h1>ReinFlow - User face</h1>
-				<h2>
-					The following will have a user end and an admin end. Graphs and stats will be displayed to users and
-					admins <br /> where admins will see an over view of everything happening.
-				</h2>
-			</div>
-		</div>
+	return ( <
+		AdminHome / >
 	);
 }
 
