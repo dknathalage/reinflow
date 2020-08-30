d5b74b280f84deab6f33a5047d26625b0c8076f4
Sheron Suditha <suditha@gamer.lk>
2020-08-30 22:42:42 +0530

    ENHANCEMENT: Base UI Update

---
+++ frontend/src/components/pages/home/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+import './admin.css';
+import ReinFlowFooter from '../../footer/footer';
+import Headers from '../../header';
+
+const { Header, Content } = Layout;
+
+export default function AdminHome() {
+	return (
+		<Layout style={{ minHeight: '100vh' }}>
+			<Headers />
+			<Layout className="site-layout">
+				<Header className="site-layout-background" style={{ padding: 0 }} />
+				<Content style={{ margin: '0 16px' }}>
+					<Breadcrumb style={{ margin: '16px 0' }}>
+						<Breadcrumb.Item>Home</Breadcrumb.Item>
+						<Breadcrumb.Item>Overview</Breadcrumb.Item>
+					</Breadcrumb>
+					<div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+						Our Overview page
+					</div>
+				</Content>
+				<ReinFlowFooter />
+			</Layout>
+		</Layout>
+	);
+}
