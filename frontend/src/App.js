806b9d8bc85c10bdaeea93935166671b352db086
Sheron Suditha <suditha@gamer.lk>
2020-08-31 10:11:49 +0530

    UPDATE: Logo dir update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import logo from '../assets/REINFLOW.png';
+import logo from './assets/REINFLOW.png';
 import './App.css';
 import AdminHome from './components/pages/home';
 import Error from './components/404';
 import Dashboard from './components/pages/dashboard';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect
+} from 'react-router-dom';
 
 function App() {
-	return (
-		<Router>
-			<div>
-				<Switch>
-					<Route path="/" exact component={AdminHome} />
-					<Route path="/dashboard" exact component={Dashboard} />
-					<Route component={Error} />
-				</Switch>
-			</div>
-		</Router>
+	return ( <
+		Router >
+		<
+		div >
+		<
+		Switch >
+		<
+		Route path = "/"
+		exact component = {
+			AdminHome
+		}
+		/> <
+		Route path = "/dashboard"
+		exact component = {
+			Dashboard
+		}
+		/> <
+		Route component = {
+			Error
+		}
+		/> <
+		/Switch> <
+		/div> <
+		/Router>
 	);
 }
 
