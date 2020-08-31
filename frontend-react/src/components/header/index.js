ca0de2bd65c2f816f504dfd2f85b1e8b6cc5a93b
Sheron Suditha <me@sheronsuditha.me>
2020-09-01 01:49:53 +0530

    UPDATE: added click handler

--- frontend-react/src/components/header/index.js
+++ frontend-react/src/components/header/index.js
@@ -1,26 +1,47 @@
 import React from 'react';
 
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Badge } from 'antd';
 import { DesktopOutlined, PieChartOutlined, FileOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
 import { NavLink } from 'react-router-dom';
 
-import { useSelector } from 'react-redux';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { set_directory } from '../../redux/actions/directory';
+import logo from '../../assets/REINFLOW.png';
+import Avatar from 'antd/lib/avatar/avatar';
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
 function Headers() {
 	const user = useSelector((state) => state.user);
+	const dir = useSelector((state) => state.dir);
+	const dispatch = useDispatch();
+
+	const handleOnClick = (e) => {
+		dispatch(
+			set_directory({
+				current_dir: 'Home',
+				dir_key: 1
+			})
+		);
+	};
+
+	const handleOnClick2 = (e) => {
+		dispatch(
+			set_directory({
+				current_dir: 'Admin',
+				dir_key: 2
+			})
+		);
+	};
 
 	return user.auth_status === true ? (
 		<div>
 			<Header style={{ position: 'fixed', zIndex: 1, width: '100%', marginLeft: '150px' }}>
-				<div className="logo" />
-				<Menu theme="dark" mode="horizontal" defaultSelectedKeys={[ '1' ]}>
-					<Menu.Item key="1">
+				<Menu theme="dark" mode="horizontal" defaultSelectedKeys={[ `${dir.dir_key}` ]}>
+					<Menu.Item key="1" onClick={handleOnClick}>
 						<NavLink to="/">Home</NavLink>
 					</Menu.Item>
-					<Menu.Item key="2">
+					<Menu.Item key="2" onClick={handleOnClick2}>
 						<NavLink to="/dashboard">Admin</NavLink>
 					</Menu.Item>
 					<Menu.Item key="3">
@@ -30,10 +51,18 @@ function Headers() {
 			</Header>
 			<Sider collapsible style={{ minHeight: '100vh' }}>
 				<div className="logo" />
-				<Menu theme="dark" defaultSelectedKeys={[ '1' ]} mode="inline" style={{ marginTop: '60px' }}>
-					<Menu.Item key="1" icon={<PieChartOutlined />}>
-						Overview
-					</Menu.Item>
+				<Menu theme="dark" defaultSelectedKeys={[ '1' ]} mode="inline" style={{ marginTop: '70px' }}>
+					<SubMenu
+						key="1"
+						icon={<Avatar size="small" icon={<UserOutlined />} />}
+						title={
+							<span style={{ color: 'white', fontWeight: 'bolder', padding: '10px' }}>
+								{user.username}
+							</span>
+						}
+					>
+						<Menu.Item key="4">User Dashboard</Menu.Item>
+					</SubMenu>
 					<Menu.Item key="2" icon={<DesktopOutlined />}>
 						Option 2
 					</Menu.Item>
@@ -41,10 +70,9 @@ function Headers() {
 						<Menu.Item key="3">Something here</Menu.Item>
 					</SubMenu>
 					<SubMenu key="sub2" icon={<TeamOutlined />} title="Sensors">
-						<Menu.Item key="4">Sensor 1</Menu.Item>
-						<Menu.Item key="5">Sensor 2</Menu.Item>
+						<Menu.Item key="5">Sensor 1</Menu.Item>
+						<Menu.Item key="6">Sensor 2</Menu.Item>
 					</SubMenu>
-					<Menu.Item key="9" icon={<FileOutlined />} />
 				</Menu>
 			</Sider>
 		</div>
