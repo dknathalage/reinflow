7adb49718df014b0e8754678d6b6665dc696a9c0
Sheron Suditha <me@sheronsuditha.me>
2020-09-01 14:04:55 +0530

    QUALITY: changed status codes and responses.

--- backend-express/routes/auth.js
+++ backend-express/routes/auth.js
@@ -1,45 +1,70 @@
 const router = require('express').Router();
 const User = require('../model/user')
-const validation = require('../functions/userValidation')
+const validation = require('../functions/validation')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 router.post('/register', async (req, res) => {
-    const {error} = validation.registerValidation(req.body)
-    if(error) return res.status(400).send(error.details[0].message)
+    console.log(req.body);
+    const {
+        error
+    } = validation.registerValidation(req.body)
+    if (error) return res.status(400).json({
+        error: error.details[0].message
+    })
 
-    const emailExists = await User.findOne({email: req.body.email})
-    if(emailExists) return res.status(400).send("email already exists")
+    const emailExists = await User.findOne({
+        email: req.body.email
+    })
+    if (emailExists) return res.status(403).json({
+        message: "Email Already Exists"
+    })
 
     const salt = await bcrypt.genSalt(10)
     const hashPassword = await bcrypt.hash(req.body.pass, salt)
 
     const user = new User({
         name: req.body.name,
-        email:req.body.email,
+        email: req.body.email,
         pass: hashPassword,
         accessLevel: 3
     })
     try {
         const savedUser = await user.save()
-        res.send(savedUser)
-    }catch(err){
-        res.status(400).send(err);
+        res.status(200).json(savedUser)
+    } catch (err) {
+        res.status(400).json({
+            error: err
+        });
     }
 })
 
 router.post('/login', async (req, res) => {
-    const {error} = validation.loginValidation(req.body)
-    if(error) return res.status(400).send(error.details[0].message)
+    const {
+        error
+    } = validation.loginValidation(req.body)
+    if (error) return res.status(403).send(error.details[0].message)
 
-    const user = await User.findOne({email: req.body.email})
-    if(!user) return res.status(400).send("email doesn't exist")
+    const user = await User.findOne({
+        email: req.body.email
+    })
+    if (!user) return res.status(403).json({
+        message: "Invalid Email"
+    })
 
     const validPass = await bcrypt.compare(req.body.pass, user.pass)
-    if(!validPass) return res.status(400).send("invalid username or password")
+    if (!validPass) return res.status(403).json({
+        message: "Invalid Password"
+    })
 
-    const token = jwt.sign({_id: user._id, access: user.accessLevel}, process.env.SECRET_TOKEN)
-    return res.header('auth-token', token).status(200).send(token)
+    const token = jwt.sign({
+        _id: user._id,
+        access: user.accessLevel
+    }, "1CC9A79BDAF09793062750A17E559D40D5C0F1372CC859F67FE1081E6BC8A307")
+    return res.header('auth_token', token).status(200).json({
+        username: user.name,
+        access_level: user.accessLevel
+    })
 })
 
 module.exports = router;
