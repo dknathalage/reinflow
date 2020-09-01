00888dc420983702f7821d40d6c73e3e0d193826
SheronSuditha <github@sudithakumarasinghe.me>
2020-09-01 20:04:47 +0530

    NEW: User Dashboard

--- frontend-react/src/components/pages/sider/user_dashboard/index.js
+++ frontend-react/src/components/pages/sider/user_dashboard/index.js
@@ -1,29 +1,57 @@
-import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import React from "react";
+import { Layout, Menu, Breadcrumb, PageHeader, Card, Row, Col } from "antd";
 
-import ReinFlowFooter from '../../../footer/footer';
-import Headers from '../../../header';
+import ReinFlowFooter from "../../../footer/footer";
+import Headers from "../../../header";
+import "./userdaash.css";
 const { Header, Content } = Layout;
 
 function UserDashboard() {
-	return (
-		<Layout style={{ minHeight: '100vh' }}>
-			<Headers />
-			<Layout className="site-layout">
-				<Header className="site-layout-background" style={{ padding: 0 }} />
-				<Content style={{ margin: '0 16px' }}>
-					<Breadcrumb style={{ margin: '16px 0' }}>
-						<Breadcrumb.Item>User</Breadcrumb.Item>
-						<Breadcrumb.Item>Dashboard</Breadcrumb.Item>
-					</Breadcrumb>
-					<div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-						User Dashboard
-					</div>
-				</Content>
-				<ReinFlowFooter />
-			</Layout>
-		</Layout>
-	);
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Headers />
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: "0 16px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+          </Breadcrumb>
+          <PageHeader
+            className="site-page-header"
+            title="User Details"
+            breadcrumb="TEST"
+            subTitle="Manage user account"
+          />
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            <div className="site-card-wrapper">
+              <Row gutter={16}>
+                <Col span={8}>
+                  <Card title="USER" bordered={true} hoverable>
+                    User
+                  </Card>
+                </Col>
+                <Col span={8}>
+                  <Card title="Card title" bordered={true} hoverable>
+                    Card content
+                  </Card>
+                </Col>
+                <Col span={8}>
+                  <Card title="Card title" bordered={true} hoverable>
+                    Card content
+                  </Card>
+                </Col>
+              </Row>
+            </div>
+          </div>
+        </Content>
+        <ReinFlowFooter />
+      </Layout>
+    </Layout>
+  );
 }
 
 export default UserDashboard;
