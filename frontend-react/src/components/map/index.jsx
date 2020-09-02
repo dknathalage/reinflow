08c3cdd80ab0e3a4cbbc428ecfb146169c706bb9
Sheron Suditha <me@sheronsuditha.me>
2020-09-02 14:27:07 +0530

    QUALITY: changed to jsx function

--- frontend-react/src/components/map/index.jsx
+++ frontend-react/src/components/map/index.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Map, TileLayer } from 'react-leaflet';
 
-export default class ReinFlowMap extends React.Component {
-    render() {
-        return <>
-            <Map center={[-37.84766,145.11486]} zoom={16}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                />
-            </Map>
-        </>
-    }
+function ReinFlowMap() {
+	return (
+		<Map center={[ -37.84766, 145.11486 ]} zoom={16}>
+			<TileLayer
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+			/>
+		</Map>
+	);
 }
+
+export default ReinFlowMap;
