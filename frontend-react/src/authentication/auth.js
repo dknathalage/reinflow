e029dde5ea2e17db76648400a97ef6ca3c28df45
Sheron Suditha <me@sheronsuditha.me>
2020-09-02 21:43:43 +0530

    UPDATE: removed cookies

--- frontend-react/src/authentication/auth.js
+++ frontend-react/src/authentication/auth.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
-import {
-    Cookies
-} from 'react-cookie'
 const API_URL_USR = "http://localhost:5000/api/user"
 export async function login_user(email, pass) {
-    const cookies = new Cookies();
     try {
         const resp = await axios.post(`${API_URL_USR}/login`, {
             email,
@@ -13,9 +9,6 @@ export async function login_user(email, pass) {
 
         if (resp.status === 200) {
             const authToken = await resp.data.token;
-            cookies.set('token', authToken);
-            cookies.set('name', resp.data.username);
-            cookies.set('accessLevel', resp.data.access_level)
             const response = await axios.get('https://ipapi.co/json/');
             const loc_data = response.data;
             return {
