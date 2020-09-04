aaff3045fa21ffbea7230311ad2154c6c170128d
Sheron Suditha <me@sheronsuditha.me>
2020-09-04 15:32:24 +0530

    FIX: Authorization header

--- backend-express/app.js
+++ backend-express/app.js
@@ -15,9 +15,9 @@ const app = express();
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
-  });
+});
 
 
 // serving static docs
