3f1d53227f9282fe0ee0d7f72d96cdc3c8bf2bbe
Sheron Suditha <me@sheronsuditha.me>
2020-09-04 14:59:40 +0530

    NEW: added lights endpoint

--- frontend-react/src/authentication/management.js
+++ frontend-react/src/authentication/management.js
@@ -3,6 +3,7 @@ import {
 	API_URL
 } from './urls'
 const API_ACCESS = `${API_URL}/api/sensors`;
+const API_ACCESS_LIGHTS = `${API_URL}/api/lights`;
 const config = {
 	headers: {
 		Authorization: localStorage.getItem('auth-token')
@@ -13,6 +14,35 @@ export async function add_new_sensor(lat, lon) {
 	try {
 		const resp = await axios.post(
 			`${API_ACCESS}/register`, {
+				lat,
+				lon
+			},
+			config
+		);
+		const data = await resp.data;
+		if (data.status === true) {
+			return {
+				status: true,
+				data: data.data
+			};
+		} else {
+			return {
+				status: false,
+				error: data.error
+			};
+		}
+	} catch (error) {
+		return {
+			status: false,
+			error
+		};
+	}
+}
+
+export async function add_new_light(lat, lon) {
+	try {
+		const resp = await axios.post(
+			`${API_ACCESS_LIGHTS}/register`, {
 				lat,
 				lon
 			},
