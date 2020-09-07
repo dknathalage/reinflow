a359d8af8eb30762c20f0da091d4a4d6daf31a96
Sheron Suditha <me@sheronsuditha.me>
2020-09-07 07:44:58 +0530

    FIX: added the new Endpoint

--- frontend-react/src/authentication/urls.js
+++ frontend-react/src/authentication/urls.js
@@ -1,1 +1,8 @@
-export let API_URL = 'https://reinflow-backend.vercel.app'
+export let API_URL = 'http://panel.sheronsuditha.me:5000'
+export const config = {
+    headers: {
+        Authorization: localStorage.getItem('auth-token')
+    }
+};
+
+export let API_GET_USERS_URL = `${API_URL}/api/info/users`
