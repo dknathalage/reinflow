5160104ea24d4e431879f5a8c577a794660f0854
Sheron Suditha <me@sheronsuditha.me>
2020-09-08 21:57:21 +0530

    UPDATE: mongoo schema update

--- backend-express/model/sensor.js
+++ backend-express/model/sensor.js
@@ -3,14 +3,12 @@ module.exports = mongoose.model('sensor', new mongoose.Schema({
     id: String,
     sensor_name: String,
     sensor_description: String,
+    lon: Number,
+    lat: Number,
     location: {
         type: {
             type: String,
             enum: ['Point'],
-            required: true
-        },
-        coordinates: {
-            type: [Number],
             required: true
         }
     }
