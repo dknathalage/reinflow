53b79d2b01ca3360d93f2f9dc37fc46011e01906
Kushan-Nilanga <dknathalage@gmail.com>
2020-09-09 12:18:08 +1000

    NEW: sprint 1 routes have been moved to secure access routes l1, l2, l3

--- backend-express/routes/accesslevel1/userinfo.js
+++ backend-express/routes/accesslevel1/userinfo.js
@@ -1,5 +1,20 @@
 const router = require('express').Router();
+const users = require('../../model/user');
 
-
+router.get('/', async (req, res) => {
+    try {
+        const allUsers = await users.find({});
+        return res.status(200).json({
+            status: true,
+            message: "All Users in the system",
+            users: allUsers
+        })
+    } catch (error) {
+        return res.status(404).json({
+            status: false,
+            message: error.message
+        })
+    }
+})
 
 module.exports = router;
--- backend-express/routes/info.js
+++ backend-express/routes/info.js
@@ -1,61 +1,69 @@
-const router = require('express').Router();
-const verify = require('../functions/verifyToken');
-const users = require('../model/user');
-const Sensor = require('../model/sensor');
-const Light = require('../model/light');
+// const router = require('express').Router();
+// const verify = require('../functions/verifyToken');
+// const users = require('../model/user');
+// const Sensor = require('../model/sensor');
+// const Light = require('../model/light');
 
-//nned to implement a looger 
-console.log("RUNNING INFO ROUTE")
+// //nned to implement a looger 
+// console.log("RUNNING INFO ROUTE")
 
-router.post('/users', (req, res, next) => verify(req, res, next, 3), async (req, res) => {
-    try {
-        const allUsers = await users.find({});
-        return res.status(200).json({
-            status: true,
-            message: "All Users in the system",
-            users: allUsers
-        })
-    } catch (error) {
-        return res.status(404).json({
-            status: false,
-            message: error.message
-        })
-    }
-})
-
-router.post('/sensors', (req, res, next) => verify(req, res, next, 3), async (req, res) => {
-    try {
-        const sensors = await Sensor.find({});
-        res.status(200).json({
-            status: true,
-            sensors: sensors
-        })
-    } catch (error) {
-        console.log(error)
-        res.send(404).json({
-            status: false,
-            error
-        })
-    }
-
-});
-
-router.post('/lights', (req, res, next) => verify(req, res, next, 3), async (req, res) => {
-    try {
-        const lights = await Light.find({})
-        res.status(200).json({
-            status: true,
-            lights
-        })
-    } catch (err) {
-        console.log(err)
-        res.status(404).json({
-            status: false,
-            error: err
-        })
-    }
-})
+/** WARNING
+ * This route has been moved permanently to accesslevel1/users.js under GET->htts://localhost:5000/api/l1/users
+ * */
+// router.post('/users', (req, res, next) => verify(req, res, next, 3), async (req, res) => {
+//     try {
+//         const allUsers = await users.find({});
+//         return res.status(200).json({
+//             status: true,
+//             message: "All Users in the system",
+//             users: allUsers
+//         })
+//     } catch (error) {
+//         return res.status(404).json({
+//             status: false,
+//             message: error.message
+//         })
+//     }
+// })
 
 
+/** WARNING
+ * This route has been moved permanently to accesslevel3/sensors.js under GET->htts://localhost:5000/api/l3/sensors 
+ * */
+// router.post('/sensors', (req, res, next) => verify(req, res, next, 3), async (req, res) => {
+//     try {
+//         const sensors = await Sensor.find({});
+//         res.status(200).json({
+//             status: true,
+//             sensors: sensors
+//         })
+//     } catch (error) {
+//         console.log(error)
+//         res.send(404).json({
+//             status: false,
+//             error
+//         })
+//     }
+// });
 
-module.exports = router;
+
+/** WARNING
+ * This route has been moved permanently to accesslevel3/light.js under GET->htts://localhost:5000/api/l3/lights
+ * */
+// router.post('/lights', (req, res, next) => verify(req, res, next, 3), async (req, res) => {
+//     try {
+//         const lights = await Light.find({})
+//         res.status(200).json({
+//             status: true,
+//             lights
+//         })
+//     } catch (err) {
+//         console.log(err)
+//         res.status(404).json({
+//             status: false,
+//             error: err
+//         })
+//     }
+// })
+
+// module.exports = router;
