6575499a2d86623679b3f5fefcdd3d28223c57b2
Sheron Suditha <me@sheronsuditha.me>
2020-09-10 21:40:19 +0530

    UPDATE: API endpoint update

--- frontend-react/src/authentication/urls.js
+++ frontend-react/src/authentication/urls.js
@@ -3,3 +3,5 @@ export let API_URL = 'http://panel.sheronsuditha.me:5000'
 export let API_GET_USERS_URL = `${API_URL}/api/l1/info/users`
 export let API_GET_SENSORS = `${API_URL}/api/l2/info/sensors`
 export let API_GET_TRAFFIC_LIGHTS = `${API_URL}/api/l2/info/lights`
+export let API_GET_SPEC_USER = `${API_URL}/api/l1/info/users`
+export let API_USER_SETLEVEL = `${API_URL}/api/l1/manage/setlevel`
