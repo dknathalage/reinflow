d0c54eb5dba9b2c7527d9f5736828383b42fa441
Sheron Suditha <me@sheronsuditha.me>
2020-09-11 01:35:34 +0530

    NEW: Get light endpoint

--- frontend-react/src/authentication/fetcher.js
+++ frontend-react/src/authentication/fetcher.js
@@ -3,7 +3,8 @@ import {
 	API_GET_USERS_URL,
 	API_GET_SENSORS,
 	API_GET_TRAFFIC_LIGHTS,
-	API_GET_SPEC_USER
+	API_GET_SPEC_USER,
+	API_GET_LIGHT
 } from './urls';
 
 export async function get_registered_users() {
@@ -85,6 +86,29 @@ export async function get_spec_user(userId) {
 			return {
 				status: true,
 				user: data.user
+			}
+		} else {
+			return {
+				status: false,
+				message: data.message
+			}
+		}
+	} catch (error) {
+		return {
+			status: false,
+			error: error.message
+		}
+	}
+}
+
+export async function get_light(lightid) {
+	try {
+		const resp = await axios.post(`${API_GET_LIGHT}/${lightid}`, null);
+		const data = await resp.data;
+		if (data.status === true) {
+			return {
+				status: true,
+				light: data.light
 			}
 		} else {
 			return {
