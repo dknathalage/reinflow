80f5d293d7dcf29e238d260ded148db417e6d614
Sheron Suditha <me@sheronsuditha.me>
2020-09-10 21:42:31 +0530

    UPDATE: refactor

--- frontend-react/src/components/map/index.jsx
+++ frontend-react/src/components/map/index.jsx
@@ -19,15 +19,18 @@ class ReinFlowMap extends React.Component {
 		this.state = { value: 0, lat: 0, lon: 0, icons: [] };
 		//this.setState({ lat: res.data.lat, lon: res.data.lon, value: res.data.status });
 
-
 		setInterval(async () => {
-			const responce = await axios(`${API_URL}/api/l3/realtime/lightbuffer`).catch(err => {
-				console.log(err)
-			})
-			//console.log(responce.data.data)
-			const icons = responce.data.data.map((value) => <TrafficLightMarker key={value._id} lat={value.lat} lon={value.lon} color={value.status} />)
-			this.setState({ icons })
-			console.log(this.state.icons)
+			const responce = await axios.get(`${API_URL}/api/l3/realtime/lightbuffer`).catch((err) => {
+				console.log(err);
+			});
+			console.log(responce.data.data);
+
+			const icons = responce.data.data.map((value) => (
+				<TrafficLightMarker key={value._id} lat={value.lat} lon={value.lon} color={value.status} />
+			));
+
+			this.setState({ icons });
+			console.log(this.state.icons);
 		}, 10000);
 	}
 
@@ -35,24 +38,18 @@ class ReinFlowMap extends React.Component {
 		clearTimeout(this.intervalID);
 	}
 
-	componentDidMount = () => {
-
-	};
+	componentDidMount = () => {};
 
 	render() {
 		return (
 			<div>
-				<Map center={[-37.84766, 145.11486]} zoom={16} style={{ zIndex: 1 }} id="iconContainer">
+				<Map center={[ -37.84766, 145.11486 ]} zoom={16} style={{ zIndex: 1 }} id="iconContainer">
 					<TileLayer
 						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 						attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-					/><div>
-						{this.state.icons.map((element) => 
-							<React.Fragment>
-								{element}
-							</React.Fragment>
-						)}</div>
-				</Map >
+					/>
+					<div>{this.state.icons.map((element) => <React.Fragment>{element}</React.Fragment>)}</div>
+				</Map>
 			</div>
 		);
 	}
