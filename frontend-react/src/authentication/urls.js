6a3db9489d98df0512e2474cba24468876f936f4
Sheron Suditha <me@sheronsuditha.me>
2020-09-10 14:36:30 +0530

    UPDATE: API route updates

--- frontend-react/src/authentication/urls.js
+++ frontend-react/src/authentication/urls.js
@@ -1,5 +1,5 @@
-export let API_URL = 'http://localhost:5000'
+export let API_URL = 'http://panel.sheronsuditha.me:5000'
 
-export let API_GET_USERS_URL = `${API_URL}/api/info/users`
-export let API_GET_SENSORS = `${API_URL}/api/info/sensors`
-export let API_GET_TRAFFIC_LIGHTS = `${API_URL}/api/info/lights`
+export let API_GET_USERS_URL = `${API_URL}/api/l1/info/users`
+export let API_GET_SENSORS = `${API_URL}/api/l2/info/sensors`
+export let API_GET_TRAFFIC_LIGHTS = `${API_URL}/api/l2/info/lights`
