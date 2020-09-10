02d7411a1433e2b1b2c89582f294e2b42c9fb6fe
Sheron Suditha <me@sheronsuditha.me>
2020-09-11 01:35:45 +0530

    NEW: get light api route

--- frontend-react/src/authentication/urls.js
+++ frontend-react/src/authentication/urls.js
@@ -5,3 +5,4 @@ export let API_GET_SENSORS = `${API_URL}/api/l2/info/sensors`
 export let API_GET_TRAFFIC_LIGHTS = `${API_URL}/api/l2/info/lights`
 export let API_GET_SPEC_USER = `${API_URL}/api/l1/info/users`
 export let API_USER_SETLEVEL = `${API_URL}/api/l1/manage/setlevel`
+export let API_GET_LIGHT = `${API_URL}/api/l1/info/light`
