6390b541f1a2a2d7b05dd1f7d84c825509e2fd45
Don Athalage <dknathalage@gmail.com>
2020-09-11 09:24:46 +1000

    UPDATE: url.js
    Changed url to deployment

--- frontend-react/src/authentication/urls.js
+++ frontend-react/src/authentication/urls.js
@@ -1,4 +1,4 @@
-export let API_URL = 'http://panel.sheronsuditha.me:5000'
+export let API_URL = 'https://reinflow-socketserver.vercel.app
 
 export let API_GET_USERS_URL = `${API_URL}/api/l1/info/users`
 export let API_GET_SENSORS = `${API_URL}/api/l2/info/sensors`
