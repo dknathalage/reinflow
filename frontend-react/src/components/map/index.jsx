b3ecc78a16618bd23b473e27468f89712c061baf
Sheron Suditha <me@sheronsuditha.me>
2020-09-17 22:54:55 +0530

    DEPRECATE: old maps UI

--- frontend-react/src/components/map/index.jsx
+++ frontend-react/src/components/map/index.jsx
@@ -4,7 +4,7 @@ import { Map, TileLayer, Marker } from 'react-leaflet';
 import TrafficLightMarker from './components/traffic-light';
 import axios from 'axios';
 import { API_URL } from '../../authentication/urls';
-
+import AntPath from 'react-leaflet-ant-path';
 class ReinFlowMap extends React.Component {
 	/**
 	 * 	color values for TrafficLightMarker
@@ -18,7 +18,7 @@ class ReinFlowMap extends React.Component {
 		super();
 		this.state = { value: 0, lat: 0, lon: 0, icons: [] };
 		//this.setState({ lat: res.data.lat, lon: res.data.lon, value: res.data.status });
-
+		/*
 		setInterval(async () => {
 			const responce = await axios.get(`${API_URL}/api/l3/realtime/lightbuffer`).catch((err) => {
 				console.log(err);
@@ -32,6 +32,7 @@ class ReinFlowMap extends React.Component {
 			this.setState({ icons });
 			console.log(this.state.icons);
 		}, 10000);
+		*/
 	}
 
 	componentWillUnmount() {
@@ -41,6 +42,8 @@ class ReinFlowMap extends React.Component {
 	componentDidMount = () => {};
 
 	render() {
+		var latlngs = [ [ -37.850702, 145.119881 ], [ -37.849874, 145.112964 ], [ -37.849628, 145.109074 ] ];
+
 		return (
 			<div>
 				<Map center={[ -37.84766, 145.11486 ]} zoom={16} style={{ zIndex: 1 }} id="iconContainer">
@@ -49,6 +52,7 @@ class ReinFlowMap extends React.Component {
 						attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
 					/>
 					<div>{this.state.icons.map((element) => <React.Fragment>{element}</React.Fragment>)}</div>
+					<AntPath positions={latlngs} />
 				</Map>
 			</div>
 		);
