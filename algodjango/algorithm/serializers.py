7fdb80fa74e687fb0dd7b179b2f3bb25bda4752a
Jake Roussis <jroussis@deakin.edu.au>
2020-09-24 02:16:28 +1000

    NEW: Add functionality to update light status based on route data

--- algodjango/algorithm/serializers.py
+++ algodjango/algorithm/serializers.py
@@ -1,13 +1,4 @@
 from rest_framework import serializers
-<<<<<<< HEAD
-from .model import RouteCoordinates
-
-
-class RouteDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= RouteCoordinates
-        fields = ("point1_longitude", "point1_latitude", "point2_longitude", "point2_latitude")
-=======
 from . models import Light, Route
 
 
@@ -20,4 +11,3 @@ class routeSerializer(serializers.ModelSerializer):
     class Meta:
         model=Route
         fields = '__all__'
->>>>>>> 0ce67eb0dd8c2485cf481a95fbdb7b56e2e5aa41
