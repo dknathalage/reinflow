44bbaba18b01d4f21df6b4cb0c4c543bd63069da
Kushan-Nilanga <dknathalage@gmail.com>
2020-09-24 18:39:13 +1000

    TEST: level3 light/ route

--- backend-express/tests/accesslevel3.test.js
+++ backend-express/tests/accesslevel3.test.js
@@ -27,10 +27,16 @@ describe('test access level 3 endpoints', () => {
     })
 
     /** ENDPOINT RELATED TESTS */
-    // base endpoint
+    // Check accessibility of endpoint for this access level
     test('test endpoint access for l3', async () => {
         const res = await axios.get('http://localhost:5000/api/l3', { headers: { authorization: token } });
         expect(res.data['route-access']).toBe(true);
+    })
+
+    test('test endpoint access for l2', async () => {
+        await axios.get('http://localhost:5000/api/l2', { headers: { authorization: token } }).catch((err) => {
+            expect(err.response.data['route-access']).toBe(false);
+        });
     })
 
     test('test endpoint access for l2', async () => {
@@ -43,6 +49,18 @@ describe('test access level 3 endpoints', () => {
         await axios.get('http://localhost:5000/api/l1', { headers: { authorization: token } }).catch((err) => {
             expect(err.response.data['route-access']).toBe(false);
         });
+    })
+
+    // access related endpoints
+    /// LIGHTS.JS FILE ENDPOINTS
+    test('test endpoint get lights', async () => {
+        const res = await axios.get('http://localhost:5000/api/l3/lights', { headers: { authorization: token } })
+        expect(res.data['status']).toBe(true)
+    })
+
+    test('test endpoint route request', async () => {
+        const res = await axios.get('http://localhost:5000/api/l3/lights/routedata/requests', { headers: { authorization: token } })
+        expect(res.data['status']).toBe(true)
     })
 
     test('remove created user', async () => {
